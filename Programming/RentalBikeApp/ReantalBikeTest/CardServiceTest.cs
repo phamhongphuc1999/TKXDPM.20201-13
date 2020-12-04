22a6707a169b6408bf502806bd8f16e5d9b82ff8
minhquangvx99 <55574673+minhquangvx99@users.noreply.github.com>
2020-12-04 16:25:16 +0700

    Add files via upload

---
+++ Programming/RentalBikeApp/ReantalBikeTest/CardServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using RentalBikeApp.Business.SQLServices;
+using RentalBikeApp.Entities.SQLEntities;
+
+namespace ReantalBikeTest
+{
+    [TestFixture]
+    class CardServiceTest
+    {
+        private CardService cardService;
+
+        [SetUp]
+        public void Setup()
+        {
+            cardService = new CardService();
+        }
+
+        [Test]
+        public void GetCardByOwnerTest()
+        {
+            Card card = cardService.GetCardByOwner(1);
+            Assert.IsNotNull(card);
+        }
+    }
+}
---
+++ Programming/RentalBikeApp/ReantalBikeTest/UserServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using RentalBikeApp.Business.SQLServices;
+using RentalBikeApp.Entities.SQLEntities;
+
+namespace ReantalBikeTest
+{
+    [TestFixture]
+    class UserServiceTest
+    {
+        private UserService userService;
+
+        [SetUp]
+        public void Setup()
+        {
+            userService = new UserService();
+        }
+
+        [Test]
+        public void InsertNewUserTest()
+        {
+            User cus = new User()
+            {
+                CardId = 1,
+                UserName = "Ngo Minh Quang",
+                AccountPassword = "minhquang",
+                AccountName ="quang.nm173326",
+                CusAddress = "Ha Noi",
+                Email = "quang.nm173326@gmail.com",
+                Phone = "0969696969",
+                Gender = "Nam",
+                AccountStatus = "enable"
+            };
+            User user = userService.InsertNewUser(cus);
+            Assert.IsNotNull(user);
+        }
+    }
+
+}
