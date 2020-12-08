502667c818212c063f91975783af6ed6df8dde36
minhquangvx99 <55574673+minhquangvx99@users.noreply.github.com>
2020-12-08 22:41:24 +0700

    Add files via upload

---
+++ Programming/RentalBikeApp/ReantalBikeTest/ElectricBikeServiceTest.cs
@@ -0,0 +1,51 @@
+﻿// --------------------RENTAL BIKE APP-----------------
+//
+//
+// Copyright (c) Microsoft. All Rights Reserved.
+// License under the Apache License, Version 2.0.
+//
+//   Su Huu Vu Quang
+//   Pham Hong Phuc
+//   Tran Minh Quang
+//   Ngo Minh Quang
+//
+//
+// ------------------------------------------------------
+
+using NUnit.Framework;
+using RentalBikeApp.Business.SQLServices;
+using RentalBikeApp.Entities.SQLEntities;
+using System;
+using System.Collections.Generic;
+
+namespace ReantalBikeTest
+{
+    [TestFixture]
+    class ElectricBikeServiceTest
+    {
+        private ElectricBikeService electricBikeService;
+        [SetUp]
+        public void Setup()
+        {
+            electricBikeService = new ElectricBikeService();
+        }
+        [Test]
+        public void GetBikeByQRCodeTest()
+        {
+            ElectricBike electricBike = electricBikeService.GetBikeByQRCode(" ");
+            Assert.IsNotNull(electricBike);
+        }
+        [Test]
+        public void GetBikeByIdTest()
+        {
+            ElectricBike electricBike = electricBikeService.GetBikeById(1);
+            Assert.IsNotNull(electricBike);
+        }
+        [Test]
+        public void GetListBikesInStationTest()
+        {
+            List<ElectricBike> electricBikes = electricBikeService.GetListBikesInStation(1);
+            Assert.IsTrue(electricBikes.Count > 0);
+        }
+    }
+}
---
+++ Programming/RentalBikeApp/ReantalBikeTest/TandemServiceTest.cs
@@ -0,0 +1,52 @@
+﻿// --------------------RENTAL BIKE APP-----------------
+//
+//
+// Copyright (c) Microsoft. All Rights Reserved.
+// License under the Apache License, Version 2.0.
+//
+//   Su Huu Vu Quang
+//   Pham Hong Phuc
+//   Tran Minh Quang
+//   Ngo Minh Quang
+//
+//
+// ------------------------------------------------------
+
+using NUnit.Framework;
+using RentalBikeApp.Business.SQLServices;
+using RentalBikeApp.Entities.SQLEntities;
+using System;
+using System.Collections.Generic;
+
+namespace ReantalBikeTest
+{
+    [TestFixture]
+    class TandemServiceTest
+    {
+        private TandemService tandemService;
+        [SetUp]
+        public void Setup()
+        {
+            tandemService = new TandemService();
+        }
+        [Test]
+        public void GetBikeByQRCodeTest()
+        {
+            Tandem tandem = tandemService.GetBikeByQRCode(" ");
+            Assert.IsNotNull(tandem);
+        }
+        [Test]
+        public void GetBikeByIdTest()
+        {
+            Tandem tandem = tandemService.GetBikeById(1);
+            Assert.IsNotNull(tandem);
+        }
+        [Test]
+        public void GetListBikesInStationTest()
+        {
+            List < Tandem > tandems = tandemService.GetListBikesInStation(1);
+            Assert.IsTrue(tandems.Count > 0);
+        }
+
+    }
+}
