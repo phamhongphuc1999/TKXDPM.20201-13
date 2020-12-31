a0b489ee32e02c1d85f32501fa58c82d90603eaf
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2020-12-31 07:57:27 +0700

    fix:

--- FinalProject/Construction/RentalBikeApp/RentalBikeApp/Entities/InterbankEntities/InterbankResponse.cs
+++ FinalProject/Construction/RentalBikeApp/RentalBikeApp/Entities/InterbankEntities/InterbankResponse.cs
@@ -1,4 +1,18 @@
-﻿namespace RentalBikeApp.Entities.InterbankEntities
+﻿// --------------------RENTAL BIKE APP-----------------
+//
+//
+// Copyright (c) Microsoft. All Rights Reserved.
+// License under the Apache License, Version 2.0.
+//
+//   Su Huu Vu Quang
+//   Pham Hong Phuc
+//   Tran Minh Quang
+//   Ngo Minh Quang
+//
+//
+// ------------------------------------------------------
+
+namespace RentalBikeApp.Entities.InterbankEntities
 {
     /// <summary>
     /// representing response from interbank
